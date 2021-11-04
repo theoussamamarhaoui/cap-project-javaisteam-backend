a81c998ed61b7ab061fa90d844a379a5f55ea966
MARHAOUI <oussama.marhaoui@capgemini.com>
2021-11-04 23:17:47 +0100

    2nd push for backend 04/11/2021 23:17

---
+++ src/main/java/com/cap/backendcapproject/controller/AdminController.java
@@ -0,0 +1,4 @@
+package com.cap.backendcapproject.controller;
+
+public class AdminController {
+}
---
+++ src/main/java/com/cap/backendcapproject/exceptions/AdminException.java
@@ -0,0 +1,8 @@
+package com.cap.backendcapproject.exceptions;
+
+public class AdminException extends RuntimeException{
+
+    public AdminException(String message) {
+        super(message);
+    }
+}
